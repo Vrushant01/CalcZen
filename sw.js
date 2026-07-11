17f00fa92c9c182757ced74e0d9ac52bb77a29e1
Vrushant01 <shiroyavrushant07@gmail.com>
2026-07-11 12:36:12 +0530

    monetag

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11268975
+    "zoneId": 11269027
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
